803092a5b169240dc22964a720cfb447062252e3
DmytroKalenko <76845911+DmytroKalenko@users.noreply.github.com>
2021-05-11 13:04:49 +0200

    FinishAdaptive

--- js/main.js
+++ js/main.js
@@ -78,22 +78,22 @@ const weather = document.querySelector('.modal_window .title .aboutweather .weat
 
 function myLocation() {
     const getLocation = (resoult) => {
-        console.log(resoult);
+        // console.log(resoult);
         let Lat = resoult.coords.latitude;
         let Lon = resoult.coords.longitude;
-        console.log(Lat, Lon);
+        // console.log(Lat, Lon);
 
         fetch(`https://api.opencagedata.com/geocode/v1/json?q=${Lat}+${Lon}&key=7a8980e030af475e9c79a7e6a0161f00`)
             .then(function(resp) { return resp.json() })
             .then(function(data2) {
-                console.log(data2);
+                // console.log(data2);
                 city = data2.results[0].components.city;
-                console.log(city);
+                // console.log(city);
 
                 fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${Lat}&lon=${Lon}&appid=51e0fc8246f5f18489d57289d2145462`)
                     .then(function(resp) { return resp.json() })
                     .then(function(data) {
-                        console.log(data);
+                        // console.log(data);
                         weather.innerHTML = city + " " + Math.round(data.main.temp - 273.15) + '&deg';
                         weatherIcon.style.cssText = `background : url(https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png) center no-repeat;
                         background-size: cover;`
